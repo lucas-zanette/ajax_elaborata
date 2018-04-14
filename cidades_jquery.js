a020e636f7d259c1e0daeee38bb19f050a12df59
lucas-zanette <lucasgz@gmail.com>
2018-04-13 21:54:07 -0300

    bkp jquery

---
+++ cidades_jquery.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function () {
+	$('button').click(function () {
+		var cidade = $('input').val();
+
+		var del = $('<span class="del">X</span>').click(function () {
+			$(this).parent()remove();
+		}
+		);
+
+		var li = $('<li>' + cidade + '</li>').append(del);
+
+		//$("ul").append('<li>'+ cidade </li>);
+		$("ul").append(li);
+	}
+	);
+
+}
+);
